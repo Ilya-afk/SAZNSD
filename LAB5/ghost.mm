f59aa2c4663ab141a70a1aef1f8eee9622bb0ea4
Ilya Vodolazky <57459526+Ilya-afk@users.noreply.github.com>
2023-05-26 19:52:46 +0300

    Add files via upload

---
+++ LAB5/ghost.mm
@@ -0,0 +1,144 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Защита информации&#xa;ОСНОВНЫЕ ТЕРМИНЫ И ОПРЕДЕЛЕНИЯ" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1685118691356" VGAP_QUANTITY="3 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#eeeeee" zoom="0.207">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_grey_straight_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1082331723" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" STYLE="rectangle" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" VGAP_QUANTITY="3 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1082331723" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="horizontal" COLOR="#444444" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#ffffff" BACKGROUND_COLOR="#3333ff" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#3333ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_334768726" COLOR="#443333" BACKGROUND_COLOR="#dddddd" BORDER_WIDTH="3 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#cc0033">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#cc0033" TRANSPARENCY="255" DESTINATION="ID_334768726"/>
+<font SIZE="12" BOLD="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#000000" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#2c2b29" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ffffff" BACKGROUND_COLOR="#222222" BORDER_COLOR="#2c2b29">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ffffff" BACKGROUND_COLOR="#444444" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#ffffff" BACKGROUND_COLOR="#676767" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ffffff" BACKGROUND_COLOR="#888888" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#222222" BACKGROUND_COLOR="#aaaaaa" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" COLOR="#222222" BACKGROUND_COLOR="#bbbbbb" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" COLOR="#222222" BACKGROUND_COLOR="#cccccc" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" COLOR="#222222" BACKGROUND_COLOR="#dddddd" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" COLOR="#222222" BACKGROUND_COLOR="#eeeeee" BORDER_COLOR="#f0f0f0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" COLOR="#2c2b29" BACKGROUND_COLOR="#ffffff" BORDER_COLOR="#f0f0f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Область применения" POSITION="bottom_or_right" ID="ID_986326731" CREATED="1685119055236" MODIFIED="1685119835365" HGAP_QUANTITY="-200.20001 pt" VSHIFT_QUANTITY="681.60002 pt" VGAP_QUANTITY="3 pt"/>
+<node TEXT=" Термины и определения" POSITION="bottom_or_right" ID="ID_440838973" CREATED="1685119076533" MODIFIED="1685119845137" HGAP_QUANTITY="212.6 pt" VSHIFT_QUANTITY="-703.80002 pt">
+<node TEXT="Общие понятия" ID="ID_167498210" CREATED="1685119087316" MODIFIED="1685119170524">
+<node TEXT="защита информации" ID="ID_1695974590" CREATED="1685119269027" MODIFIED="1685119277995"/>
+</node>
+<node TEXT="Термины, относящиеся к видам защиты информации" ID="ID_1060376192" CREATED="1685119091942" MODIFIED="1685119183593">
+<node TEXT="правовая защита информации" ID="ID_1253219124" CREATED="1685119293523" MODIFIED="1685119306414"/>
+<node TEXT="техническая защита информации" ID="ID_1847528984" CREATED="1685119296947" MODIFIED="1685119312942"/>
+<node TEXT="криптографическая защита информации" ID="ID_1328711742" CREATED="1685119297389" MODIFIED="1685119318778"/>
+<node TEXT="физическая защита информации" ID="ID_48645811" CREATED="1685119297842" MODIFIED="1685119324199"/>
+</node>
+<node TEXT="Термины, относящиеся к способам защиты&#xa;информации" ID="ID_1458445913" CREATED="1685119092237" MODIFIED="1685119193982">
+<node TEXT="способ защиты информации" ID="ID_451818785" CREATED="1685119336774" MODIFIED="1685119348748"/>
+<node TEXT="защита информации от утечки" ID="ID_1548330496" CREATED="1685119339014" MODIFIED="1685119354022"/>
+<node TEXT="защита информации от несанкционированного воздействи" ID="ID_47269392" CREATED="1685119339309" MODIFIED="1685119361756"/>
+<node TEXT="защита информации от непреднамеренного воздействия" ID="ID_1448623193" CREATED="1685119339725" MODIFIED="1685119367730"/>
+<node TEXT="защита информации от разглашения" ID="ID_1610641178" CREATED="1685119340114" MODIFIED="1685119374000"/>
+<node TEXT="защита информации от несанкционированного доступа" ID="ID_1235008846" CREATED="1685119340487" MODIFIED="1685119382825"/>
+<node TEXT="защита информации от преднамеренного воздействия" ID="ID_1431971459" CREATED="1685119340855" MODIFIED="1685119391647"/>
+<node TEXT="защита информации от [иностранной] разведки" ID="ID_331396118" CREATED="1685119341242" MODIFIED="1685119403667"/>
+</node>
+<node TEXT="Термины, относящиеся к замыслу защиты&#xa;информации" ID="ID_509837115" CREATED="1685119092578" MODIFIED="1685119202876">
+<node TEXT="замысел защиты информации" ID="ID_1589475837" CREATED="1685119427263" MODIFIED="1685119440286"/>
+<node TEXT="цель защиты информации" ID="ID_764888137" CREATED="1685119430780" MODIFIED="1685119445368"/>
+<node TEXT="система защиты информации" ID="ID_1113689813" CREATED="1685119431124" MODIFIED="1685119450116"/>
+<node TEXT="политика безопасности (информации в организации)" ID="ID_1439417938" CREATED="1685119431530" MODIFIED="1685119456901"/>
+<node TEXT="безопасность информации [данных]" ID="ID_564202464" CREATED="1685119431894" MODIFIED="1685119464007"/>
+</node>
+<node TEXT="Термины, относящиеся к объекту защиты&#xa;информации" ID="ID_363528893" CREATED="1685119092889" MODIFIED="1685119208646">
+<node TEXT="объект защиты информации" ID="ID_1921650926" CREATED="1685119473742" MODIFIED="1685119485206"/>
+<node TEXT="защищаемая информация" ID="ID_283235215" CREATED="1685119475179" MODIFIED="1685119490373"/>
+<node TEXT="носитель защищаемой информации" ID="ID_889315433" CREATED="1685119475550" MODIFIED="1685119495778"/>
+<node TEXT="защищаемый объект информатизации" ID="ID_69924127" CREATED="1685119475918" MODIFIED="1685119501733"/>
+<node TEXT="защищаемая информационная система" ID="ID_186798132" CREATED="1685119476252" MODIFIED="1685119507507"/>
+</node>
+<node TEXT="Термины, относящиеся к угрозам безопасности&#xa;информации" ID="ID_1409864256" CREATED="1685119093144" MODIFIED="1685119216163">
+<node TEXT="угроза (безопасности информации)" ID="ID_96910753" CREATED="1685119515311" MODIFIED="1685119524937"/>
+<node TEXT="фактор, воздействующий на защищаемую информацию" ID="ID_1723649212" CREATED="1685119515835" MODIFIED="1685119530536"/>
+<node TEXT="источник угрозы безопасности информации" ID="ID_319536132" CREATED="1685119516143" MODIFIED="1685119535275"/>
+<node TEXT="уязвимость (информационной системы)" ID="ID_1710917994" CREATED="1685119516443" MODIFIED="1685119541246"/>
+<node TEXT="вредоносная программа" ID="ID_1142360337" CREATED="1685119554142" MODIFIED="1685119568085"/>
+<node TEXT="несанкционированное воздействие на информацию" ID="ID_223688124" CREATED="1685119554523" MODIFIED="1685119573275"/>
+<node TEXT="преднамеренное силовое электромагнитное воздействие на&#xa;информацию" ID="ID_1932568963" CREATED="1685119554865" MODIFIED="1685119578669"/>
+<node TEXT="модель угроз (безопасности информации)" ID="ID_1614766844" CREATED="1685119555202" MODIFIED="1685119585514"/>
+</node>
+<node TEXT="Термины, относящиеся к технике защиты информации" ID="ID_1241988424" CREATED="1685119093497" MODIFIED="1685119234785">
+<node TEXT="техника защиты информации" ID="ID_642010604" CREATED="1685119593633" MODIFIED="1685119603543"/>
+<node TEXT="средство защиты информации" ID="ID_1705801689" CREATED="1685119595373" MODIFIED="1685119608882"/>
+<node TEXT="средство контроля эффективности защиты информации" ID="ID_530406183" CREATED="1685119595624" MODIFIED="1685119615669"/>
+<node TEXT="средство физической защиты информации" ID="ID_1172484327" CREATED="1685119595829" MODIFIED="1685119622469"/>
+<node TEXT="криптографическое средство защиты информации" ID="ID_765415325" CREATED="1685119596327" MODIFIED="1685119627918"/>
+</node>
+<node TEXT="Термины, относящиеся к способам оценки&#xa;соответствия требованиям по защите информации" ID="ID_940911073" CREATED="1685119113917" MODIFIED="1685119242435">
+<node TEXT="оценка соответствия требованиям по защите информации" ID="ID_1162026534" CREATED="1685119640584" MODIFIED="1685119653245"/>
+<node TEXT="лицензирование в области защиты информации" ID="ID_109125130" CREATED="1685119641341" MODIFIED="1685119659979"/>
+<node TEXT="сертификация на соответствие требованиям по безопасности&#xa;информации" ID="ID_975955756" CREATED="1685119641538" MODIFIED="1685119665122"/>
+<node TEXT="специальная проверка" ID="ID_544948411" CREATED="1685119641712" MODIFIED="1685119674821"/>
+<node TEXT="аудиторская проверка информационной безопасности в&#xa;организации" ID="ID_948144014" CREATED="1685119641897" MODIFIED="1685119681473"/>
+<node TEXT="мониторинг безопасности информации" ID="ID_271730562" CREATED="1685119642397" MODIFIED="1685119688355"/>
+</node>
+<node TEXT="Термины, относящиеся к эффективности защиты&#xa;информации" ID="ID_953069470" CREATED="1685119114855" MODIFIED="1685119251240">
+<node TEXT="эффективность защиты информации" ID="ID_1589777676" CREATED="1685119694988" MODIFIED="1685119703718"/>
+<node TEXT="требование по защите информации" ID="ID_1837954221" CREATED="1685119695629" MODIFIED="1685119709436"/>
+<node TEXT="показатель эффективности защиты информации" ID="ID_1583415230" CREATED="1685119695886" MODIFIED="1685119714972"/>
+<node TEXT="норма эффективности защиты информации" ID="ID_104631660" CREATED="1685119696246" MODIFIED="1685119721367"/>
+</node>
+</node>
+</node>
+</map>
